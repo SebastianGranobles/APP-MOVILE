4c122f71d2394de474ad97bd5fa1198db8713576
Sebastian Granobles Ardila <sebastiangranobles@hotmail.com>
2026-01-21 22:20:58 -0500

    small change

--- Parcial_SebastianGranoblesArdila/Parcial_SebastianGranoblesArdila/app/src/test/java/com/example/parcial_sebastiangranoblesardila/utils/MyFirebaseMessagingService.kt
+++ Parcial_SebastianGranoblesArdila/Parcial_SebastianGranoblesArdila/app/src/test/java/com/example/parcial_sebastiangranoblesardila/utils/MyFirebaseMessagingService.kt
@@ -7,8 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import androidx.core.content.getSystemService
-import androidx.privacysandbox.tools.core.generator.build
 import com.example.parcial_sebastiangranoblesardila.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
